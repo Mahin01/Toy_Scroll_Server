48f64575145fca7709afb21b43bdbc5b31c3c5b3
Mahin01 <Ashmahin4@gmail.com>
2023-05-21 09:56:21 +0600

    All Toys routes created & fetched data from MongoDB database

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const carDataByCat = require("./fakeData/carsDataByCat.json");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
+app.use(express.json())
 
-const carDataByCat = require("./fakeData/carsDataByCat.json");
-
-app.get("/", (req, res) => {
-  res.send("Toy Scrolls server Running");
-});
-
-app.get("/cars-by-category", (req, res) => {
-  res.send(carDataByCat);
-});
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hcrmfrb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,23 +18,41 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const allToysCollection = client.db("toy-scroll").collection("all-toys");
+
+    app.get("/all-toys", async (req, res) => {
+      const cursor = allToysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Toy Scrolls server Running");
+});
+
+app.get("/cars-by-category", (req, res) => {
+  res.send(carDataByCat);
+});
 
 app.listen(port, () => {
   console.log(`Toy Scrolls running on port: ${port}`);
