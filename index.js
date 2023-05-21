11125f8bb9a26980b22673283a522cfb1411a3ce
Mahin01 <Ashmahin4@gmail.com>
2023-05-21 21:52:07 +0600

    My Toys API created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const carDataByCat = require("./fakeData/carsDataByCat.json");
 require("dotenv").config();
 const app = express();
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hcrmfrb.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,24 +34,37 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/toy-details/:id', async (req, res) => {
+    app.get("/toy-details/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
 
       const options = {
-          // Include only the `title` and `imdb` fields in the returned document
-          projection: { 
-            seller_name: 1, price: 1, seller_email :1, 
-            ToyName:1, quantity:1, Photo_URL: 1 },
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {
+          seller_name: 1,
+          price: 1,
+          seller_email: 1,
+          ToyName: 1,
+          quantity: 1,
+          Photo_URL: 1,
+        },
       };
 
       const result = await allToysCollection.findOne(query, options);
       res.send(result);
     });
 
-    
+    app.get("/my-toys", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+      query = { seller_email: req.query.email }
+      }
+      console.log(query);
+      const result = await allToysCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.post('/add-toy', async (req, res) => {
+    app.post("/add-toy", async (req, res) => {
       const newToy = req.body;
       console.log(newToy);
       const result = await allToysCollection.insertOne(newToy);
