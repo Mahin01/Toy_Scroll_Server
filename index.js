acc15a155cf81c82de21fc4b7ca89e1f4015a72d
Mahin01 <Ashmahin4@gmail.com>
2023-05-21 13:55:20 +0600

    Add toy data send to MongoDB database implemented

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ async function run() {
       res.send(result);
     });
 
+    app.post('/add-toy', async (req, res) => {
+      const newToy = req.body;
+      console.log(newToy);
+      const result = await allToysCollection.insertOne(newToy);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
